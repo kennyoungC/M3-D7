628d56f27dbb46e28f2191ef93fa326929c1765c
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-11 17:35:38 +0300

    display all names completed

--- index.js
+++ index.js
@@ -1,51 +1,77 @@
 `use strict`;
-
+const tbody = document.querySelector(`tbody`);
 const getUsers = async () => {
   const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
   const response = await usersData.json();
-  const tbody = document.querySelector(`tbody`);
   tbody.innerHTML = ``;
   response.forEach((element) => {
     tbody.innerHTML += users(element);
   });
 };
+
 const filterUserEmail = async () => {
-  const query = document.querySelector(`input`).value;
-  const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
-  const response = await usersData.json();
-  const tbody = document.querySelector(`tbody`);
-  tbody.innerHTML = ``;
-  response
-    .filter((elem) => elem.email.toLowerCase().includes(query.toLowerCase()))
-    .forEach((element) => {
-      tbody.innerHTML += users(element);
-    });
+  try {
+    const query = document.querySelector(`input`).value;
+    const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await usersData.json();
+    const tbody = document.querySelector(`tbody`);
+    tbody.innerHTML = ``;
+    response
+      .filter((elem) => elem.email.toLowerCase().includes(query.toLowerCase()))
+      .forEach((element) => {
+        tbody.innerHTML += users(element);
+      });
+  } catch (err) {
+    console.error(err);
+  }
 };
 const filterUserName = async () => {
-  const query = document.querySelector(`input`).value;
-  const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
-  const response = await usersData.json();
-  const tbody = document.querySelector(`tbody`);
-  tbody.innerHTML = ``;
-  response
-    .filter((elem) => elem.username.toLowerCase().includes(query.toLowerCase()))
-    .forEach((element) => {
-      tbody.innerHTML += users(element);
-    });
+  try {
+    const query = document.querySelector(`input`).value;
+    const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await usersData.json();
+    tbody.innerHTML = ``;
+    response
+      .filter((elem) =>
+        elem.username.toLowerCase().includes(query.toLowerCase())
+      )
+      .forEach((element) => {
+        tbody.innerHTML += users(element);
+      });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 const filterName = async () => {
-  const query = document.querySelector(`input`).value;
-  const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
-  const response = await usersData.json();
-  const tbody = document.querySelector(`tbody`);
-  tbody.innerHTML = ``;
-  response
-    .filter((elem) => elem.name.toLowerCase().includes(query.toLowerCase()))
-    .forEach((element) => {
-      tbody.innerHTML += users(element);
-    });
+  try {
+    const query = document.querySelector(`input`).value;
+    const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await usersData.json();
+    const tbody = document.querySelector(`tbody`);
+    tbody.innerHTML = ``;
+    response
+      .filter((elem) => elem.name.toLowerCase().includes(query.toLowerCase()))
+      .forEach((element) => {
+        tbody.innerHTML += users(element);
+      });
+  } catch (err) {
+    console.error(err);
+  }
 };
-
+const displayAllNames = async () => {
+  try {
+    const ol = document.querySelector(`ol`);
+    if (ol.children.length > 0) return;
+    const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await usersData.json();
+    const names = response.map((el) => el.name);
+    names.forEach((name) => {
+      ol.innerHTML += `<li>${name}</li>`;
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 const users = (data) => ` <tr>
 <th scope="row">${data.id}</th>
 <td>${data.name}</td>
@@ -56,4 +82,5 @@ const users = (data) => ` <tr>
 </tr>`;
 window.onload = () => {
   getUsers();
+  // displayAllNames();
 };
